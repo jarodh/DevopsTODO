ff04f004b3762083c441cef0ee944cafe105874e
Jarod Hogan <hogan.jarod@gmail.com>
2023-08-31 06:43:35 -0500

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DevopsTODO;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DevopsTODO;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
